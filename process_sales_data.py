2d60f7b35532d7f073db70a0105c9f025315f3d1
TristanSweat <105946649+TristanSweat@users.noreply.github.com>
2022-06-10 15:20:40 -0400

    Update get_order_dir & split_sales_into_orders
    added in script for get_order_dir
    added in script for split_sales_into_orders.
    Need to finish formatting

--- process_sales_data.py
+++ process_sales_data.py
@@ -1,6 +1,8 @@
 from sys import argv, exit
 import os
 from datetime import date
+import pandas as pd
+import re
 
 def get_sales_csv():
 
@@ -18,10 +20,10 @@ def get_sales_csv():
         print('Error: No CSV file path provided')
         exit('Script execution aborted')
 
-def get_order_dir(sales_sata_csv):
+def get_order_dir(sales_data_csv):
     
     #Get directory path of sales data CSV file
-    sales_dir = os.path.dirname(sales_sata_csv)
+    sales_dir = os.path.dirname(sales_data_csv)
 
     #Determine ordersdirectory name (Orders_YYYY-MM-DD):
     todays_date = date.today().isoformat()
@@ -36,7 +38,44 @@ def get_order_dir(sales_sata_csv):
 
     return order_dir
 
+def split_sales_into_orders(sales_data_csv, order_dir):
+
+    # Read data from ther sales data CSV into a DataFrame
+    sales_df = pd.read_csv(sales_data_csv)
+
+    #Inserting new column for total price
+    sales_df.insert(7, 'TOTAL PRICE', sales_df['ITEM QUANTITY'] * sales_df['ITEM PRICE'])
+
+    #Drop unwanted coumns
+    sales_df.drop(columns=['ADDRESS', 'CITY', 'STATE', 'POSTAL CODE', 'COUNTRY'], inplace=True)
+
+    for order_id, order_df in sales_df.groupby('ORDER ID'):
+
+        #Drop the order ID Column
+        order_df.drop(columns=['ORDER ID'], inplace=True)
+
+        #Sort the order by item number
+        order_df.sort_values(by='ITEM NUMBER', inplace=True)
+
+        #Add grand total row at the bottom
+        grand_total = order_df['TOTAL PRICE'].sum()
+        grand_total_df = pd.DataFrame({'ITEM PRICE': ['GRAND TOTAL:'], 'TOTAL PRICE': [grand_total]})
+        order_df = pd.concat([order_df, grand_total_df])
+
+        #Determine the save path of the order file
+        customer_name = order_df['CUSTOMER NAME'].values[0]
+        customer_name = re.sub(r'\W', '', customer_name)
+        order_file_name = 'Order' + str(order_id) + '_' + customer_name + '.xlsx'
+        order_file_path = os.path.join(order_dir, order_file_name)
+
+        #Save the order information to an Excel spreadsheet
+        sheet_name = 'Order #' + str(order_id)
+        order_df.to_excel(order_file_path, index=False, sheet_name=sheet_name)
+
+     
+
+
 
 sales_data_csv = get_sales_csv()
-print(sales_data_csv)
 order_dir = get_order_dir(sales_data_csv)
+split_sales_into_orders(sales_data_csv, order_dir)
